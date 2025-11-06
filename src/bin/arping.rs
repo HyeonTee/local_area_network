c1b9884d3bea29638af083519e727cd5211841fa
HyeonTee <gusxo1820@icloud.com>
2025-11-06 02:21:21 +0000

    (arping) socket binding, send

--- src/bin/arping.rs
+++ src/bin/arping.rs
@@ -1,3 +1,82 @@
+use local_area_network::l2::{if_arp::ArpHdr, if_ether::EthHdr};
+
+fn parse_args() -> [u8;4] {
+	let args = std::env::args().collect::<Vec<String>>();
+	if args.len() != 2 {
+		panic!("args.len() should be 2");
+	}
+
+	let dest_ip = args[1].split(".").collect::<Vec<&str>>();
+	if dest_ip.len() != 4 {
+		panic!("Invalid dest ip");
+	}
+
+	let dest_ip: [u8;4] = dest_ip.iter().map(|bit| bit.parse::<u8>().unwrap()).collect::<Vec<u8>>().try_into().unwrap();
+
+	dest_ip
+}
+
 fn main() {
-	println!("arping");
+	let dest_ip = parse_args();
+	// socket
+	let sock_fd = unsafe { libc::socket(libc::AF_PACKET, libc::SOCK_RAW, libc::ETH_P_ARP.to_be()) }; /* protocol should be big-endian */
+	if sock_fd < 0 {
+		panic!("socket failed")
+	}
+
+	// eth0 -> index
+	let interface = default_net::get_default_interface().unwrap();
+	let if_index = interface.index as i32;
+	let src_mac = interface.mac_addr.unwrap().octets();
+	let src_ip = interface.ipv4[0].addr.octets();
+
+	// socket-interface binding
+	let mut sockaddr: libc::sockaddr_ll = unsafe { std::mem::zeroed() }; 	/* sockaddr_ll : link layer */ 
+	sockaddr.sll_family = libc::AF_PACKET as u16;
+	sockaddr.sll_ifindex = if_index as i32;
+	sockaddr.sll_protocol = (libc::ETH_P_ARP as u16).to_be();				/* protocol should be big-endian */
+
+	let bind_result = unsafe {
+		libc::bind(
+			sock_fd,
+			&sockaddr as *const libc::sockaddr_ll as *const libc::sockaddr, /* &sockaddr_ll -> *const sockaddr_ll -> *const sockaddr */
+			std::mem::size_of::<libc::sockaddr_ll>() as libc::socklen_t
+		)
+	};
+
+	if bind_result < 0 {
+		panic!("bind failed");
+	}
+
+	send_arp_request(sock_fd, dest_ip, src_ip, src_mac);
+}
+
+fn send_arp_request(sock_fd: i32, dest_ip: [u8; 4], src_ip: [u8; 4], src_mac: [u8; 6]) {
+	// Create L2 Frame
+	let mut buf = [0u8; 42];
+	let (eth_hdr, arp_hdr) = buf.split_at_mut(libc::ETH_HLEN as usize);
+
+	let eth_hdr = unsafe { &mut *(eth_hdr.as_mut_ptr() as *mut EthHdr) };
+	let arp_hdr = unsafe { &mut *(arp_hdr.as_mut_ptr() as *mut ArpHdr) };
+
+	// mac addr
+	eth_hdr.h_dest = [0xff; libc::ETH_ALEN as usize];
+	eth_hdr.h_source = src_mac;
+	eth_hdr.h_proto = (libc::ETH_P_ARP as u16).to_be();
+
+	// arp header
+	arp_hdr.ar_hrd = libc::ARPHRD_ETHER.to_be();
+	arp_hdr.ar_pro = (libc::ETH_P_IP as u16).to_be();
+	arp_hdr.ar_hln = libc::ETH_ALEN as u8;
+	arp_hdr.ar_pln = 4;
+	arp_hdr.ar_op = libc::ARPOP_REQUEST.to_be();
+	arp_hdr.ar_sha = src_mac;
+	arp_hdr.ar_sip = src_ip;
+	arp_hdr.ar_tha = [0x00; libc::ETH_ALEN as usize];
+	arp_hdr.ar_tip = dest_ip;
+
+	let send_bytes = unsafe { libc::send(sock_fd, buf.as_ptr() as *const libc::c_void, buf.len(), 0) };
+	if send_bytes < 0 {
+		panic!("send failed");
+	}
 }
--- src/l2/if_ether.rs
+++ src/l2/if_ether.rs
@@ -2,11 +2,11 @@
 pub struct EthHdr {
 	pub h_dest: [u8; libc::ETH_ALEN as usize],
 	pub h_source: [u8; libc::ETH_ALEN as usize],
-	pub p_proto: u16
+	pub h_proto: u16
 }
 
 impl EthHdr {
-	pub fn net(buf: &mut [u8]) -> &mut Self {
-		unsafe {&mut *(buf.as_mut_ptr() as *mut Self)}
+	pub fn new(buf: &mut [u8]) -> &mut Self {
+		unsafe { &mut *(buf.as_mut_ptr() as *mut Self) }
 	}
 }
